3cf4fec36549780e2f6fdb6f9513085476f83b98
javad <javadhoseinzadeh80@gmail.com>
2022-06-19 15:49:27 +0430

    kavenegar error

--- sms/urls.py
+++ sms/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DeatailSick
+from .views import DeatailSick, send_sms
 app_name = "sms-app"
 
 urlpatterns = [
-    path("<slug:slug>/", DeatailSick.as_view(), name="h")
+    path("<slug:slug>/", DeatailSick.as_view(), name="h"),
+    path("send-sms", send_sms, name="send_sms")
 ]
--- sms/views.py
+++ sms/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
+from kavenegar import *
 
 from Forms.models import info
-from django.views.generic import DeleteView
+from django.views.generic import DeleteView, ListView
 
 class DeatailSick(DeleteView):
     template_name = "forms/detailsick.html"
@@ -10,9 +11,39 @@ class DeatailSick(DeleteView):
         slug = self.kwargs.get("slug")
         infoss = info.objects.filter(slug=slug)
         return info.objects.filter(slug=slug)
-"""recivers = []
+
 def send_sms(request):
-    for sms in info.objects.all():
-        recivers.append(sms.mobile)
-        print(recivers)
-send_text(subject, message, from_email, )"""
+    try:
+        api = KavenegarAPI('{63414D33743579696D783334664754374853574554632B2F79336834576A5874467766427577346C364D493D}')
+        params = {
+            'sender': '1000551451',#optional
+            'receptor': '09105675936',#multiple mobile number, split by comma
+            'message': "عزیز به هلسی خوش اومدی ما حالتو بهتر میکنیم",
+        } 
+        response = api.sms_send(params)
+        print(response)
+    except APIException as e: 
+        print(e)
+    except HTTPException as e: 
+        print(e)
+
+    return render(request, "forms/sms-sender.html")
+
+
+"""class send_sms(ListView):
+    template_name = "forms/sms-sender.html"
+    def get_queryset(self):
+        api = KavenegarAPI("{63414D33743579696D783334664754374853574554632B2F79336834576A5874467766427577346C364D493D}")
+        recivers = []
+        for sms in info.objects.all():
+            recivers.append(sms.mobile)
+            print(recivers)
+        params = {
+            'sender': '1000551451',
+            'receptor' : '09105675936',
+            'message' : "f'{sms.slug}', عزیز به هلسی خوش اومدی ما حالتو بهتر میکنیم f'{sms.slug}'"
+        }   
+        response = api.sms_send(params)
+
+
+"""

bf84ae409b1c70e29e6c6a70b97b30e6d4c0d1ca
javad <javadhoseinzadeh80@gmail.com>
2022-06-25 11:33:47 +0430

    kavenegar message

--- sms/views.py
+++ sms/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from kavenegar import *
-
 from Forms.models import info
 from django.views.generic import DeleteView, ListView
 
@@ -14,13 +13,16 @@ class DeatailSick(DeleteView):
 
 def send_sms(request):
     try:
-        api = KavenegarAPI('{63414D33743579696D783334664754374853574554632B2F79336834576A5874467766427577346C364D493D}')
+        api = KavenegarAPI('63414D33743579696D783334664754374853574554632B2F79336834576A5874467766427577346C364D493D')
         params = {
-            'sender': '1000551451',#optional
-            'receptor': '09105675936',#multiple mobile number, split by comma
-            'message': "عزیز به هلسی خوش اومدی ما حالتو بهتر میکنیم",
-        } 
-        response = api.sms_send(params)
+            'receptor': '09105675936',
+            'template': 'VerifyRegistrationWeb',
+            'token': 'جواد',
+            'token2': 'https:127.0.0.1/124-145',
+            'type': 'sms',#sms vs call
+        }
+
+        response = api.verify_lookup(params)
         print(response)
     except APIException as e: 
         print(e)
@@ -30,20 +32,19 @@ def send_sms(request):
     return render(request, "forms/sms-sender.html")
 
 
-"""class send_sms(ListView):
-    template_name = "forms/sms-sender.html"
-    def get_queryset(self):
-        api = KavenegarAPI("{63414D33743579696D783334664754374853574554632B2F79336834576A5874467766427577346C364D493D}")
-        recivers = []
+"""    try:
+        api = KavenegarAPI('')
         for sms in info.objects.all():
-            recivers.append(sms.mobile)
-            print(recivers)
+            a = sms.slug
+            b = sms.fname
         params = {
-            'sender': '1000551451',
-            'receptor' : '09105675936',
-            'message' : "f'{sms.slug}', عزیز به هلسی خوش اومدی ما حالتو بهتر میکنیم f'{sms.slug}'"
-        }   
+            'sender': '1000551451',#optional
+            'receptor': '09105675936',#multiple mobile number, split by comma
+            'message': "f'{self.b}'عزیز، به هلسی خوش آمدید ما حالتو بهتر میکنیم f'{self.a}'",
+        } 
         response = api.sms_send(params)
-
-
-"""
+        print(response)
+    except APIException as e: 
+        print(e)
+    except HTTPException as e: 
+        print(e)"""
